b5b664111dd4b3e6cfc5e351d71446101df0061d
NayanaBR <97843806+NayanaBR@users.noreply.github.com>
2022-01-16 22:40:47 +0530

    Add files via upload

---
+++ Write.java
@@ -0,0 +1,23 @@
+package N;
+package FileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Write {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+	FileWriter f=new FileWriter("C:\\\\Users\\\\rashm\\\\abc.txt");
+
+	f.write("welcome to java");
+	f.close();
+	System.out.println("succesfilly updated");
+	}
+		catch(IOException e) {
+		e.printStackTrace();
+	}
+	}
+
+}
